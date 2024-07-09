f66946b8f0a2607f42514cb0e7f49a6a4229eec4
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-09 15:08:05 +0000

    Updated placeholders

--- components/header-component.js
+++ components/header-component.js
@@ -10,16 +10,16 @@ Vue.component("header-component", {
         <div class="max-w-6xl mx-auto px-4 sm:px-6">
             <div class="flex items-center justify-between h-16 md:h-20">
     
-                <div id="site-branding" class="flex-1"><a id="logo-link" class="inline-flex" href="index.html" aria-label="Cruip"><img id="logo-image" class="max-w-none" src="./images/logo.svg" width="38" height="38" alt="Stellar"></a></div>
+                <div id="site-branding" class="flex-1"><a id="logo-link" class="inline-flex" href="index.html" aria-label="Cruip"><img id="logo-image" class="max-w-none" src="static/img/generate-an-image-for-a-logo-for-the-website-based-on-the-project-description-create-a-keyboard-trading-market.png" width="38" height="38" alt="Stellar"></a></div>
     
                 <nav id="desktop-navigation" class="hidden md:flex md:grow">
     
                     <ul id="desktop-menu-links" class="flex grow justify-center flex-wrap items-center">
-                        <li><a id="HEADER_NAVBAR_TEXT_1-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" :class="[textStylePrimary, hoverTextStylePrimary]" href="HEADER_NAVBAR_TEXT_1.html">HEADER_NAVBAR_TEXT_1</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_2-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_2.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_2</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_3-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_3.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_3</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_4-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_4.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_4</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_5-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_5.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_5</a></li>
+                        <li><a id="Buy-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" :class="[textStylePrimary, hoverTextStylePrimary]" href="Buy.html">Buy</a></li>
+                        <li><a id="Sell-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Sell.html" :class="[textStylePrimary, hoverTextStylePrimary]">Sell</a></li>
+                        <li><a id="Trade-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Trade.html" :class="[textStylePrimary, hoverTextStylePrimary]">Trade</a></li>
+                        <li><a id="Community-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Community.html" :class="[textStylePrimary, hoverTextStylePrimary]">Community</a></li>
+                        <li><a id="Learn-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Learn.html" :class="[textStylePrimary, hoverTextStylePrimary]">Learn</a></li>
                     </ul>
     
                 </nav>
@@ -42,11 +42,11 @@ Vue.component("header-component", {
     
                     <nav id="mobile-nav" class="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" x-ref="mobileNav" :style="expanded ? 'max-height: ' + $refs.mobileNav.scrollHeight + 'px; opacity: 1' : 'max-height: 0; opacity: .8'" @click.outside="expanded = false" @keydown.escape.window="expanded = false" x-cloak>
                         <ul id="mobile-nav-list" class="border border-transparent rounded-lg px-4 py-1.5" :class="signUpButtonGradient">
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_1-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_1.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_1</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_2-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_2.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_2</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_3-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_3.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_3</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_4-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_4.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_4</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_5-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_5.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_5</a></li>
+                            <li><a id="mobile-Buy-link" class="flex font-medium text-sm py-1.5" href="Buy.html" :class="[textStylePrimary,hoverTextStylePrimary]">Buy</a></li>
+                            <li><a id="mobile-Sell-link" class="flex font-medium text-sm py-1.5" href="Sell.html" :class="[textStylePrimary,hoverTextStylePrimary]">Sell</a></li>
+                            <li><a id="mobile-Trade-link" class="flex font-medium text-sm py-1.5" href="Trade.html" :class="[textStylePrimary,hoverTextStylePrimary]">Trade</a></li>
+                            <li><a id="mobile-Community-link" class="flex font-medium text-sm py-1.5" href="Community.html" :class="[textStylePrimary,hoverTextStylePrimary]">Community</a></li>
+                            <li><a id="mobile-Learn-link" class="flex font-medium text-sm py-1.5" href="Learn.html" :class="[textStylePrimary,hoverTextStylePrimary]">Learn</a></li>
                         </ul>
                     </nav>
     
